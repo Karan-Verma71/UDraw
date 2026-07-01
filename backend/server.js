5e3d0304ddc716edcce1f2f3e7a3cc00df13cdaa
Karan <karanverma18072004@gmail.com>
2026-07-01 11:05:08 +0530

    added new cors restriction

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ connectToDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000", "https://udraw.vercel.app/"], 
+      origin: ["http://localhost:5173", "https://udraw.vercel.app/" , "http://localhost:3000"], 
       methods: ["GET", "POST"],
     },
   });
